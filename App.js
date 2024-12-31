e8d65ac142048d1946df08a08dc741b41c10dd5b
tung <thanhtung230323@gmail.com>
2024-12-31 14:04:19 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <KeyboardAvoidingView>
+      <View style={{ borderBottomWidth:1}}>
+       <Text style={{fontSize:20,fontWeight:1}}>Đăng nhập </Text>
+       </View>
+      <View style={styles.view1}> 
+        <Text style={{fontSize:20 , margin:20}}>Nhập số điện thoại
+
+        </Text>
+        <Text style={{margin:20,}}>Dùng số điện thoại để đăng nhập hoặc đăng ký tài khoản tại OneHousing Pro</Text>
+        <TextInput style={{borderBottomWidth:1,margin:20,top:20}} placeholder='Nhập số điện thoại của bạn' keyboardType='numeric' ></TextInput>
+       <View style={{top:300 , fontSize:30,borderRadius:5}}>
+        <Button  title='Tiếp tục'  keyboardType="numeric"  />
+        <Text></Text>
+        </View>
+      </View>
+      
       <StatusBar style="auto" />
+      </KeyboardAvoidingView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    top:50,
+    margin:10,
+   
   },
+  view1:{
+    top:100,
+    fontSize:40,
+  }
 });
